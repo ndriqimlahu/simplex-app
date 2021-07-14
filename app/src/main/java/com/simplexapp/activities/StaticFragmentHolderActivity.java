07b1cbf6dae9ab5e7cdb931a561e9f0f08764c19
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2021-07-14 15:37:37 +0200

    Added the file StaticFragmentHolderActivity.java in activities folder

---
+++ app/src/main/java/com/simplexapp/activities/StaticFragmentHolderActivity.java
@@ -0,0 +1,13 @@
+package com.simplexapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.simplexapp.R;
+
+public class StaticFragmentHolderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_fragment_holder);
+    }
+}
